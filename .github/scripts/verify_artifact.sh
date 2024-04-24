783b43eece82d6fe378041162a4d37f7650738d0
Mathias Brulatout <m.brulatout@criteo.com>
2024-04-24 19:56:24 +0200

    ci: remove rpm and deb support

--- .github/scripts/verify_artifact.sh
+++ .github/scripts/verify_artifact.sh
@@ -44,12 +44,8 @@ function main {
   fi
 
   # match against the various artifact names:
-  # deb packages: consul_${version}-1_${arch}.deb
-  # rpm packages: consul-${version}-1.${arch}.rpm
   # zip packages: consul_${version}_${os}_${arch}.zip
   case "${artifact_path}" in
-    *.rpm) verify_rpm "${artifact_path}" "${expect_version}";;
-    *.deb) verify_deb "${artifact_path}" "${expect_version}";;
     *.zip) verify_zip "${artifact_path}" "${expect_version}";;
     *)
       echo "${artifact_path} did not match known patterns"
@@ -58,99 +54,6 @@ function main {
   esac
 }
 
-# Arguments:
-#   $1 - path to rpm (eg. consul-1.13.0~dev-1.aarch64.rpm)
-#   $2 - expected version to match against (eg. v1.13.0-dev)
-function verify_rpm {
-  local artifact_path="${1:-}"
-  local expect_version="${2:-}"
-  local docker_image
-  local docker_platform
-
-  case "${artifact_path}" in
-    *.i386.rpm)
-      docker_platform="linux/386"
-      docker_image="i386/centos:7"
-      ;;
-    *.x86_64.rpm)
-      docker_platform="linux/amd64"
-      docker_image="amd64/centos:7"
-      ;;
-    *.armv7hl.rpm)
-      docker_platform="linux/arm/v7"
-      docker_image="arm32v7/fedora:36"
-      ;;
-    *.aarch64.rpm)
-      docker_platform="linux/arm64"
-      docker_image="arm64v8/fedora:36"
-      ;;
-    *)
-      echo "${artifact_path} did not match known patterns for rpms"
-      exit 1
-      ;;
-  esac
-
-  echo "executing RPM verification in Docker with these parameters:"
-  echo "PLATFORM=${docker_platform}"
-  echo "IMAGE=${docker_image}"
-
-  docker run \
-      --platform=${docker_platform} \
-      -v $(pwd):/workdir \
-      -v ${SCRIPT_DIR}:/scripts \
-      -w /workdir \
-    ${docker_image} \
-    /scripts/verify_rpm.sh \
-    "/workdir/${artifact_path}" \
-    "${expect_version}"
-}
-
-# Arguments:
-#   $1 - path to deb (eg. consul_1.13.0~dev-1_arm64.deb)
-#   $2 - expected version to match against (eg. v1.13.0-dev)
-function verify_deb {
-  local artifact_path="${1:-}"
-  local expect_version="${2:-}"
-  local docker_image
-  local docker_platform
-
-  case "${artifact_path}" in
-    *_i386.deb)
-      docker_platform="linux/386"
-      docker_image="i386/debian:bullseye"
-      ;;
-    *_amd64.deb)
-      docker_platform="linux/amd64"
-      docker_image="amd64/debian:bullseye"
-      ;;
-    *_armhf.deb)
-      docker_platform="linux/arm/v7"
-      docker_image="arm32v7/debian:bullseye"
-      ;;
-    *_arm64.deb)
-      docker_platform="linux/arm64"
-      docker_image="arm64v8/debian:bullseye"
-      ;;
-    *)
-      echo "${artifact_path} did not match known patterns for debs"
-      exit 1
-      ;;
-  esac
-
-  echo "executing DEB verification in Docker with these parameters:"
-  echo "PLATFORM=${docker_platform}"
-  echo "IMAGE=${docker_image}"
-
-  docker run \
-      --platform=${docker_platform} \
-      -v $(pwd):/workdir \
-      -v ${SCRIPT_DIR}:/scripts \
-      -w /workdir \
-    ${docker_image} \
-    /scripts/verify_deb.sh \
-    "/workdir/${artifact_path}" \
-    "${expect_version}"
-}
 
 # Arguments:
 #   $1 - path to zip (eg. consul_1.13.0-dev_linux_amd64.zip)
